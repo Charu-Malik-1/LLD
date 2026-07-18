134fcb17e6ed46187c9cde11724843f115b05ff0
Charu Malik <charu.nitjsr@gmail.com>
2026-07-18 14:57:46 +0530

    stock broker

---
+++ src/test/java/lld/stock/broker/ApplicationTests.java
@@ -0,0 +1,13 @@
+package lld.stock.broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
